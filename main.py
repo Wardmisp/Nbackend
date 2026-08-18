69c493733b41ab946ff8eaef112ecbd0a606df76
Rémi ME <44639493+Wardmisp@users.noreply.github.com>
2026-08-18 16:38:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,38 @@ def health():
         "status": "ok"
     }
 
+///TEMPORARY
+import requests
+@app.get("/test-nba")
+def test_nba():
+    url = "https://stats.nba.com/stats/playercareerstats?PlayerID=2544"
 
+    headers = {
+        "Host": "stats.nba.com",
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/131.0.0.0 Safari/537.36"
+        ),
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://www.nba.com/",
+        "Origin": "https://www.nba.com",
+        "Connection": "keep-alive",
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=60,
+    )
+
+    return {
+        "status_code": response.status_code,
+        "length": len(response.text),
+        "data": response.json(),
+    }
+////  
 @app.get("/random-player-season")
 def random_player_season():
 
